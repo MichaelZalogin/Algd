0e38af42b44937199f3ac014cf8c5f7d248d757a
Michael Z <zaloginmiha@gmail.com>
2021-12-29 23:01:59 +0300

    no comments

--- src/main/java/L5/Lesson5.java
+++ src/main/java/L5/Lesson5.java
@@ -24,14 +24,18 @@ class Car {
         speed += a;
         return speed;
     }
-        int tormoz ( int a) {
-            speed -= a;
-            return speed;
-        }
+
+    int tormoz(int a) {
+        speed -= a;
+        return speed;
+    }
+
     void showinfo() {
         System.out.println("cvet " + color + " motor: " + engine + " skorost " + speed);
     }
-}class CarTest {
+}
+
+class CarTest {
     public static void main(String[] args) {
         Car f = new Car();
         f.color = "white";
