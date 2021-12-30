61288ccc82c2bd379989acdd4c5061e8d738fbb5
Michael Z <zaloginmiha@gmail.com>
2021-12-31 00:46:39 +0300

    no comments

--- src/main/java/L6/Beer.java
+++ src/main/java/L6/Beer.java
@@ -1,13 +1,14 @@
 package L6;
 
 public class Beer {
-    public static void main(String[] args){
-    int beerNum = 5;
-    String word = "бутылок (бутылки)";
+    public static void main(String[] args) {
+        int beerNum = 5;
+        String word = "бутылок (бутылки)";
         while (beerNum > 0) {
 
-        if (beerNum == 1) {
-            word = "бутылка";}
+            if (beerNum == 1) {
+                word = "бутылка";
+            }
 
             System.out.println(beerNum + " " + word + " пива на стене");
             System.out.println("Возьми одну и выпей.");
