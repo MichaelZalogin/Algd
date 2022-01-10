06b7f70dc11192ec32658dded4a14d1493096eb1
Michael Z <zaloginmiha@gmail.com>
2022-01-10 04:08:07 +0300

    no comments

--- src/main/java/L14/L14.java
+++ src/main/java/L14/L14.java
@@ -1,4 +1,12 @@
 package L14;
 
 public class L14 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Lesson loop");
+        }
+        for (int z = 1; z < 11; z++) {
+            System.out.println(z);
+        }
+    }
 }
