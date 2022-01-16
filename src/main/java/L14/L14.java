1982e200729162405dc324f51da07590cf17c2d1
Michael Z <zaloginmiha@gmail.com>
2022-01-16 03:01:56 +0300

    no comments

--- src/main/java/L14/L14.java
+++ src/main/java/L14/L14.java
@@ -8,5 +8,22 @@ public class L14 {
         for (int z = 1; z < 11; z++) {
             System.out.println(z);
         }
+        for (int x = 4, a = 3, b = 12; x <= b && x >= 4 || b < a; x = x + 2, a = a + 1, method_for_loop(10)) {
+//            method_for_loop(10);
+            System.out.println(x + " " + a);
+        }
+    }
+
+    static void method_for_loop(int n) {
+        System.out.println(n);
+    }
+}
+
+class L15 {
+    public static void main(String[] args) { // Здесь работает loop с выводом четных чисел до 30
+        for (int i = 1; i<30; i++){
+            if (i%2==0)
+            System.out.println(i);
+        }
     }
 }
