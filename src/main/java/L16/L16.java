6110263bbe74d683d33b2060f1a0fc68bfd01e42
Michael Z <zaloginmiha@gmail.com>
2022-01-19 02:14:01 +0300

    no comments

--- src/main/java/L16/L16.java
+++ src/main/java/L16/L16.java
@@ -11,22 +11,25 @@ class Car {
         this.color = color;
         this.engine = engine;
     }
+
     final static int a = 5;
-    public Car abc (String cvet) {
-     Car c10 = new Car(cvet, "V4");
-     return c10;
+
+    public Car abc(String cvet) {
+        Car c10 = new Car(cvet, "V4");
+        return c10;
     }
 
     public static void main(String[] args) {
-        Car x = new Car("red","V6");// 35:10
+        Car x = new Car("red", "V6");// 35:10
         Car c2 = x.abc("black");
         System.out.println(x.color);
     }
 }
 
 class TestCar {
-    final static Car c = new Car ("red", "V8");
+    final static Car c = new Car("red", "V8");
+
     public static void main(String[] args) {
-       c.color = "black";
+        c.color = "black";
     }
 }
