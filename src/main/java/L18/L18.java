384ee86cfd7ee2041ec1df1b47cc109601056465
Michael Z <zaloginmiha@gmail.com>
2022-01-31 01:15:20 +0300

    no comments

--- src/main/java/L18/L18.java
+++ src/main/java/L18/L18.java
@@ -8,7 +8,7 @@ public class L18 {
         while (c < s.length() - 1) {
             a = s.indexOf('@', c);
             b = s.indexOf('.', c);
-            c = s.indexOf(';', c+1);
+            c = s.indexOf(';', c + 1);
             System.out.println(s.substring(a + 1, b));
         }
     }
@@ -16,5 +16,12 @@ public class L18 {
     public static void main(String[] args) {
         L18 obj = new L18();
         obj.email("ya@yahoo.com; on@mail.ru; ona@gmail.com;");
+    }
+}
+class Lesson18{
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder("Good Day!");
+        StringBuilder sb3 = new StringBuilder(50);
     }
 }
