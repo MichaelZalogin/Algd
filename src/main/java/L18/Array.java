93858f43e8ab8124dcf3551edf8bd5a00908f9da
Michael Z <zaloginmiha@gmail.com>
2022-02-01 02:29:40 +0300

    no comments

---
+++ src/main/java/L18/Array.java
@@ -0,0 +1,14 @@
+package L18;
+
+public class Array {
+    public static void main(String[] args) {
+        int[][] array1;//Declaration - null
+        String[][] array2;//Declaration - null
+        double[] array3;//Declaration - null
+
+        array1 = new int[5][6];// Allocation
+        array2 = new String[2][6];// Allocation
+        array3 = new double[3]; // Allocation
+
+    }
+}
