845bac6d725c9fbe554289f411fcd218e6a9fabd
Michael Z <zaloginmiha@gmail.com>
2022-02-13 00:37:32 +0300

    no comments

--- src/main/java/L20/L20P1.java
+++ src/main/java/L20/L20P1.java
@@ -24,15 +24,31 @@ public class L20P1 {
         }
         System.out.println(" ");
         ArrayList<StringBuilder> list2 = new ArrayList<>();
+        ArrayList<StringBuilder> list3 = new ArrayList<>();
         StringBuilder sb1 = new StringBuilder("Builder1");
+        StringBuilder sb4 = new StringBuilder("Builder1");
         StringBuilder sb2 = new StringBuilder("Builder2");
+        StringBuilder sb5 = new StringBuilder("Builder2");
         StringBuilder sb3 = new StringBuilder("Builder3");
+        StringBuilder sb6 = new StringBuilder("Builder3");
         list2.add(sb1);
         list2.add(sb2);
         list2.add(sb3);
+        list3.add(sb4);
+        list3.add(sb5);
+        list3.add(sb6);
+
         for (int i = 0; i < list2.size(); i++) {
             list2.get(i).append("!!!");
         }
+        for (StringBuilder s : list2) {
+            System.out.print(s + " ");
+        }
+        System.out.println(" ");
+        list2.remove(1);
+        list2.remove(sb3);
+        list2.addAll(list3);
+        list2.addAll(2,list3);
         for (StringBuilder s : list2) {
             System.out.print(s + " ");
         }
