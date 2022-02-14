1eb2cb5f2fd710cdc5888adc485dc4090fe6cacb
Michael Z <zaloginmiha@gmail.com>
2022-02-14 22:25:12 +0300

    no comments

--- src/main/java/L20/L20P2.java
+++ src/main/java/L20/L20P2.java
@@ -12,14 +12,18 @@ public class L20P2 {
         sb1.add(new StringBuilder("StringBuilder1"));
         sb1.add(new StringBuilder("StringBuilder2"));
         sb1.add(new StringBuilder("StringBuilder3"));
-        System.out.println(sb1.indexOf(new StringBuilder("StringBuilder1")));
-        System.out.println(s1.indexOf("String2"));
-        for (String st1 : s1) {
-            System.out.print(st1 + " ");
-        }
-        System.out.println(" ");
-        for (StringBuilder sb2 : sb1) {
-            System.out.print(sb2 + " ");
-        }
+//        System.out.println(sb1.indexOf(new StringBuilder("StringBuilder1")));
+//        System.out.println(s1.indexOf("String2"));
+//        System.out.println(s1.lastIndexOf("String2"));
+//        System.out.println(s1.contains("String2"));
+//        System.out.println(s1.isEmpty());
+//        System.out.println(s1.size());
+//        System.out.println(s1.toString());
+//        System.out.println(sb1.toString());
+        ArrayList<StringBuilder> sb2 = (ArrayList<StringBuilder>) sb1.clone();
+        System.out.println(sb2 == sb1);
+        System.out.println(sb1.get(0) == sb2.get(0));
+        sb1.get(0).append("!!!");
+        System.out.println(sb1.get(0));
     }
 }
