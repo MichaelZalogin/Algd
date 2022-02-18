01dbd2ebe63078d20eff1ea890cddf9e076db9ff
Michael Z <zaloginmiha@gmail.com>
2022-02-18 22:47:15 +0300

    no comments

--- src/main/java/L20/L20.java
+++ src/main/java/L20/L20.java
@@ -14,6 +14,8 @@ public class L20 {
         list1.add(s1);
         list1.add(new StringBuilder("ok"));
         ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("poka");
+        list2.add("privet");
 //        list2.add(s1);
         ArrayList<String> list3 = new ArrayList();
         ArrayList<StringBuilder> list4 = new ArrayList();
