295cc372acfb9d4290899c14e6527dadb577e4e1
Michael Z <zaloginmiha@gmail.com>
2022-02-25 01:32:04 +0300

    no comments

---
+++ src/main/java/L23/L23.java
@@ -0,0 +1,20 @@
+package L23;
+
+public class L23 {
+}
+
+class Employee {
+
+    double salary;
+    String name;
+    int age;
+    int experience;
+
+    void eat() {
+        System.out.println("Есть");
+    }
+
+    void sleep() {
+        System.out.println("Спать");
+    }
+}
