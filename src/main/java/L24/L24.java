d16103e695104faec4f5cc4821d41ebae0cc1972
Michael Z <zaloginmiha@gmail.com>
2022-02-28 01:52:15 +0300

    no comments

--- src/main/java/L24/L24.java
+++ src/main/java/L24/L24.java
@@ -3,17 +3,28 @@ package L24;
 public class L24 {
 }
 
-class Figure {
+abstract class Figure {
     int number_of_sides;
 
-    void area() {
-    }
+    abstract void area();
 
-    void perimeter() {
+    abstract void perimeter();
+
+    void show_info() {
+        System.out.println("This is figure");
     }
 }
 
 class Square extends Figure {
+    int sides = 10;
+
+    public void perimeter() {
+        System.out.println("Периметр квадрата равен: " + 4 * sides);
+    }
+
+    public void area() {
+        System.out.println("Площадь квадрата равна: " + sides * sides);
+    }
 }
 
 class Rectangle extends Figure {
