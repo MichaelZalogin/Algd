60c41893fc1da5e88fd4f17ff62f08231d17057c
Michael Z <zaloginmiha@gmail.com>
2022-02-28 01:38:06 +0300

    no comments

---
+++ src/main/java/L24/L24.java
@@ -0,0 +1,23 @@
+package L24;
+
+public class L24 {
+}
+
+class Figure {
+    int number_of_sides;
+
+    void area() {
+    }
+
+    void perimeter() {
+    }
+}
+
+class Square extends Figure {
+}
+
+class Rectangle extends Figure {
+}
+
+class Triangle extends Figure {
+}
