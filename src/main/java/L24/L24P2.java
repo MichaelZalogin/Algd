9efc160b8a1bd8e57e1d3f59d24a3d25eba72f8d
Michael Z <zaloginmiha@gmail.com>
2022-03-01 01:15:02 +0300

    no comments

---
+++ src/main/java/L24/L24P2.java
@@ -0,0 +1,27 @@
+package L24;
+
+public class L24P2 {}
+    interface i1 {
+        void abc ();
+        default void def () {
+            System.out.println("DEF");
+        }
+        default void ghi () {
+            System.out.println("GHI");
+        }
+    }
+
+class Z2 implements i1 {
+    @Override
+    public void abc() {
+        System.out.println("ABC");
+    }
+
+    public static void main(String[] args) {
+        Z2 z = new Z2();
+        z.abc();
+        z.def();
+        z.ghi();
+
+    }
+}
