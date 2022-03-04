c923b98df3817ea29458edc6617132b0ad107f5f
Michael Z <zaloginmiha@gmail.com>
2022-03-04 23:29:54 +0300

    no comments

--- src/main/java/L25/HW25.java
+++ src/main/java/L25/HW25.java
@@ -1,23 +1,24 @@
 package L25;
-public class HW25 {}
 
-interface Speakable {
-    default void speak (){
-        System.out.println("Somebody speaks");
-    };
+public class HW25 {
 }
 
 abstract class Animal {
-    Animal (String name) {
+
+    Animal(String name) {
         this.name = name;
     }
+
     String name;
-    abstract void eat ();
-    abstract void sleep ();
+
+    abstract void eat();
+
+    abstract void sleep();
 }
 
 abstract class Fish extends Animal {
-        Fish(String name) {
+
+    Fish(String name) {
         super(name);
         this.name = name;
     }
@@ -27,18 +28,53 @@ abstract class Fish extends Animal {
         System.out.println("Fish sleeps");
     }
 
-    abstract void swim ();
+    abstract void swim();
 }
 
 abstract class Bird extends Animal implements Speakable {
-    Bird (String name){
-        super (name);
+
+    Bird(String name) {
+        super(name);
         this.name = name;
     }
 
-    abstract void fly ();
+    abstract void fly();
 
-    public void speak () {
+    public void speak() {
         System.out.println(name + " sings");
     }
- }
+}
+
+interface Speakable {
+    default void speak() {
+        System.out.println("Somebody speaks");
+    }
+}
+
+abstract class Mammal extends Animal implements Speakable {
+
+    public Mammal(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    abstract void run();
+}
+
+class Mechenosec extends Fish {
+
+    public Mechenosec(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Ribiy korm");
+    }
+
+    @Override
+    void swim() {
+        System.out.println("Bistro plavaet");
+    }
+}
