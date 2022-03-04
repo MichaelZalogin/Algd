1fe8c1e70e96451787bf6493427287497f4dbdc2
Michael Z <zaloginmiha@gmail.com>
2022-03-04 23:22:25 +0300

    no comments

--- src/main/java/L25/HW25.java
+++ src/main/java/L25/HW25.java
@@ -1,6 +1,12 @@
 package L25;
 public class HW25 {}
 
+interface Speakable {
+    default void speak (){
+        System.out.println("Somebody speaks");
+    };
+}
+
 abstract class Animal {
     Animal (String name) {
         this.name = name;
@@ -15,4 +21,24 @@ abstract class Fish extends Animal {
         super(name);
         this.name = name;
     }
+
+    @Override
+    void sleep() {
+        System.out.println("Fish sleeps");
+    }
+
+    abstract void swim ();
 }
+
+abstract class Bird extends Animal implements Speakable {
+    Bird (String name){
+        super (name);
+        this.name = name;
+    }
+
+    abstract void fly ();
+
+    public void speak () {
+        System.out.println(name + " sings");
+    }
+ }
