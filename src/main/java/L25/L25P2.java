d25bb60cdc3a9b39275b6e36d2631832a4dc22d8
Michael Z <zaloginmiha@gmail.com>
2022-03-06 02:03:47 +0300

    no comments

---
+++ src/main/java/L25/L25P2.java
@@ -0,0 +1,87 @@
+package L25;
+
+public class L25P2 {
+    public static void main(String[] args) {
+        Employee1 e1 = new Doctor1();
+        Employee1 e2 = new Teacher1();
+        Employee1 e3 = new Driver1();
+        Doctor1 d1 = (Doctor1) e1;
+        System.out.println(d1.specializacia);
+        System.out.println(((Doctor1) e1).specializacia);
+        ((Doctor1) e1).heal();
+    }
+}
+
+class Employee1 {
+    double salary = 100;
+    String name;
+    int age;
+    int experience;
+
+    void eat() {
+        System.out.println("Eat");
+    }
+
+    void sleep() {
+        System.out.println("Sleep");
+    }
+}
+
+class Doctor1 extends Employee1 {
+    String specializacia = "Hirurg";
+
+    void heal() {
+        System.out.println("Heal");
+    }
+}
+
+class Teacher1 extends Employee1 implements Help_able1 {
+    int amount_student;
+
+    void teach() {
+        System.out.println("Teach");
+    }
+
+    @Override
+    public void resuscitation() {
+        System.out.println("A");
+    }
+
+    @Override
+    public void put_out_the_fire() {
+        System.out.println("B");
+    }
+}
+
+class Driver1 extends Employee1 implements Help_able1, Swim_able1 {
+    String car_name;
+
+    @Override
+    public void resuscitation() {
+        System.out.println("A");
+    }
+
+    @Override
+    public void put_out_the_fire() {
+        System.out.println("B");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("C");
+    }
+
+    void drive() {
+        System.out.println("Drive");
+    }
+}
+
+interface Help_able1 {
+    void resuscitation();
+
+    void put_out_the_fire();
+}
+
+interface Swim_able1 {
+    void swim();
+}
