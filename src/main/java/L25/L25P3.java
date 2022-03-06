437fa63ce1f19c09fcb30f0a0518003ef110bf09
Michael Z <zaloginmiha@gmail.com>
2022-03-07 01:30:21 +0300

    no comments

--- src/main/java/L25/L25P3.java
+++ src/main/java/L25/L25P3.java
@@ -10,7 +10,13 @@ class Widening {
         byte b2 = 3;// narrowing
         short s1 = -6;// narrowing
         char c1 = 100;// narrowing
-        final int i2 = 3;
+        final int i2 = 33;
         short s2 = i2;
+        int i3 = 3;
+        short s3 = (short)i3;//casting
+        long l1 = 1;
+        int i4 = (int)l1; //casting
+        char c10 = (char) -8;
+        System.out.println(c10);
     }
 }
