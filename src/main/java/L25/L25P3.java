1ad6ba6cd46c182f240843dd74d2afe4f881e543
Michael Z <zaloginmiha@gmail.com>
2022-03-07 02:14:19 +0300

    no comments

--- src/main/java/L25/L25P3.java
+++ src/main/java/L25/L25P3.java
@@ -1,6 +1,7 @@
 package L25;
 
-public class L25P3 {}
+public class L25P3 {
+}
 
 class Widening {
     public static void main(String[] args) {
@@ -13,10 +14,35 @@ class Widening {
         final int i2 = 33;
         short s2 = i2;
         int i3 = 3;
-        short s3 = (short)i3;//casting
+        short s3 = (short) i3;//casting
         long l1 = 1;
-        int i4 = (int)l1; //casting
+        int i4 = (int) l1; //casting
         char c10 = (char) -8;
         System.out.println(c10);
+        int i12 = (int) 3.14;
+        System.out.println(i12);
+        double d3 = 45535344.434;
+        byte b5 = (byte) d3;
+        System.out.println(b5);
+        int i5 = 2147483647;
+        System.out.println(i5 + 1);
+    }
+}
+
+class Numeric_promotion {
+    public static void main(String[] args) {
+        int i1 = 5;
+        long l1 = 10;
+        System.out.println(i1 + l1);
+        i1 *= l1;
+        float f1 = 3.14f;
+        int i2 = 10;
+        System.out.println(f1 + i2);
+
+        byte b1 = 3;
+        short s1 = 4;
+        char c = 5;
+        System.out.println(b1 + s1 + c);
+        System.out.println(b1++);
     }
 }
