190e76d4c093f5165de32b4ab724c5efbcab66f5
Michael Z <zaloginmiha@gmail.com>
2022-03-21 01:04:17 +0300

    no comments

--- src/main/java/L26/L26.java
+++ src/main/java/L26/L26.java
@@ -2,11 +2,12 @@ package L26;
 
 public class L26 {
     public static void main(String[] args) {
-        int i = 1;
+        int i = 2;
         int b = 4;
+        int c = 5;
         while (i <= 8) {
-            if ( (i % 2) == 0 )
-            continue;
+            if ( c < 7 )
+            i =+ 1;
             System.out.println(i);
             i++;
         }
