b6d3e276128aeaa1b1a479573d752d3dfd5a5da8
Michael Z <zaloginmiha@gmail.com>
2022-06-07 02:45:46 +0300

    Lesson2. Primitive Data Types

---
+++ src/main/java/L2/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package L2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte b1 = 1;
+        byte b2 = 127;
+
+        short s1 = 1;
+        short s2 = -32;
+
+        int i1 = 21;
+        int i2 = -43;
+
+        long l1 = 1000000L;
+        long l2 = 120032L;
+
+        float f1 = 3.14F;
+        float f2 = 3.2434f;
+
+        double d1 = 5.5;
+        double d2 = 54.0;
+
+    }
+}
