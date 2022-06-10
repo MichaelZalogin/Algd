0312ce14a1c49007aab2c7ed0c212e793adc6179
Michael Zalogin <zaloginmiha@gmail.com>
2022-06-10 15:56:33 +0300

    Lesson 3. Types of operations

--- src/main/java/L3/TypesOfOperations.java
+++ src/main/java/L3/TypesOfOperations.java
@@ -14,7 +14,7 @@ public class TypesOfOperations {
         System.out.println(a%b);
         double a2 = 11.5;
         double b2 = 3.2;
-        double c2 = a2%b2;
+        double c2 = a2 % b2;
         System.out.println(c2++);
         System.out.println(c2);
         int z = a + ++b;
@@ -23,7 +23,21 @@ public class TypesOfOperations {
         z += 12;
         z /= 11;
         System.out.println(z);
-        boolean bo1 = a>b;
+        boolean bo1 = a > b;
         System.out.println(bo1);
+        boolean a5 = true;
+        boolean b5 = false;
+        System.out.println( (a5 && b5) || a5 );
+        int f = 15;
+        int g = 13;
+        boolean c5 = f < g || f == g++;
+        // Первое условие  false, провряем второе. В этом случае g увеличилась до 15
+        boolean c6 = f > g || f == g++;
+        // Первое условие  true, второе не проверяется. В этом случае g не увеличивается.
+        boolean c7 = f > g || f == g++;
+        // Выполняются оба условия независимо от результата. В этом случае g увеличивается.
+        int d = 9;
+        int l = 12;
+        System.out.println(d & l); //побитовые операции
     }
 }
