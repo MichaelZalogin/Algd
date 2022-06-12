8bfed446108a190e3694a54d5f80d50893dc4bed
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-12 03:13:55 +0300

    Lesson 4. Create object

--- src/main/java/L4/Car.java
+++ src/main/java/L4/Car.java
@@ -11,6 +11,12 @@ class CarTest {
         Car car2 = new Car();
         Car car3 = car2;
         Car car4;
+        car1.color = "Black";
+        car1.engine = "V8";
+        System.out.println("Цвет: " + car1.color);
+        System.out.println("Мотор: " + car1.engine);
+        String name2 = new String("name");
         System.out.println(car3.color + " \n" + car3.engine);
+        System.out.println(new Car().color);
     }
 }
