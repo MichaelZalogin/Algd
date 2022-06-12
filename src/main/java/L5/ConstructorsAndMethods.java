c7c758188765c627334dc7d9c048cae30b5092d0
Michael Zalogin <zaloginmiha@gmail.com>
2022-06-12 15:02:29 +0300

    Lesson 5. Constructors and methods

--- src/main/java/L5/ConstructorsAndMethods.java
+++ src/main/java/L5/ConstructorsAndMethods.java
@@ -1,3 +1,17 @@
 package L5;
 
-public class ConstructorsAndMethods {}
+public class ConstructorsAndMethods {
+}
+
+class Test20 {
+    int summa(int a, int b, int c) {
+        int result = a + b + c;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Test20 t1 = new Test20();
+        int d = t1.summa(4, 5, 6);
+        System.out.println(d);
+    }
+}
