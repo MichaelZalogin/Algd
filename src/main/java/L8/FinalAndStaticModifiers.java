a98f41737dfa0b597e341177960d783fbc404615
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-17 02:25:01 +0300

    Lesson 8. Final and static modifiers

--- src/main/java/L8/FinalAndStaticModifiers.java
+++ src/main/java/L8/FinalAndStaticModifiers.java
@@ -3,11 +3,11 @@ package L8;
 public class FinalAndStaticModifiers {
     public final int a;
 
-    FinalAndStaticModifiers(){
-        a=10;
+    FinalAndStaticModifiers() {
+        a = 10;
     }
 
-    FinalAndStaticModifiers (boolean b) {
+    FinalAndStaticModifiers(boolean b) {
         a = 20;
     }
 
@@ -17,5 +17,21 @@ public class FinalAndStaticModifiers {
 
 //        t1.a *=2;
         System.out.println(t1.a);
+    }
+}
+
+class Car2 {
+    String color;
+    String engine;
+}
+
+class Human1 {
+    String name;
+    Car2 c1 = new Car2();
+
+    public static void main(String[] args) {
+        Human1 person = new Human1();
+        person.c1.color = "Black";
+        person.c1.engine = "V6";
     }
 }
