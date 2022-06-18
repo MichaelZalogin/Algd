d85799a66071987215cf94811f4ce64ef93feccd
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-19 00:15:17 +0300

    Lesson 10. Subpackages and import

---
+++ src/main/java/L10/L10.java
@@ -0,0 +1,17 @@
+package L10;
+
+import java.lang.*;
+
+import L8.HomeworkL8;
+import L9.Car;
+import L6.*;
+
+public class L10 {
+
+    public static void main(String[] args) {
+        HomeworkL8 h1 = new HomeworkL8();
+        Car c1 = new Car("Blue", "V8");
+        ConstructorOverloading k1 = new ConstructorOverloading();
+        L10 m1 = new L10();
+    }
+}
