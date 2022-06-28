7dda94091c9eb23be85c691cde68363a00ab2434
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-28 22:04:18 +0300

    Lesson 16. Class String

---
+++ src/main/java/L16/Txt.java
@@ -0,0 +1,16 @@
+package L16;
+
+public class Txt {
+    public static void main(String[] args) {
+        String s1 = "Чтоб мудро жизнь прожить, знать надобно не мало, Два важных правила запомни для начала: Ты " +
+                "лучше голодай, чем что попало есть, И лучше будь один, чем вместе с кем попало.";
+
+        char c1;
+        char c2;
+
+        for (int i = 0; i > s1.length(); i++) {
+            c1 = s1.charAt(i);
+            c2 = s1.charAt(i);
+        }
+    }
+}
