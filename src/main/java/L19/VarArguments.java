0d75782bb02376a3757c635ef054e4b3f1bb77bc
Michael Zalogin <zaloginmiha@gmail.com>
2022-07-01 18:28:53 +0300

    Lesson 19. Variable arguments

--- src/main/java/L19/VarArguments.java
+++ src/main/java/L19/VarArguments.java
@@ -17,6 +17,10 @@ public class VarArguments {
         }
     }
 
+    public static void abc(int[]... a) {
+        //some code
+    }
+
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         intAgrs(array1);
