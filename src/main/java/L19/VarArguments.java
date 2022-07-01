4bfa1c6fbddccad288839b2efa17bbe506abfde0
Michael Zalogin <zaloginmiha@gmail.com>
2022-07-01 18:38:21 +0300

    Lesson 19. Variable arguments

--- src/main/java/L19/VarArguments.java
+++ src/main/java/L19/VarArguments.java
@@ -5,7 +5,7 @@ public class VarArguments {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
-            System.out.println(sum);
+            System.out.print(sum + " ");
         }
     }
 
@@ -13,7 +13,7 @@ public class VarArguments {
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
             sum += a[i];
-            System.out.println(sum);
+            System.out.print(sum + " ");
         }
     }
 
@@ -24,6 +24,16 @@ public class VarArguments {
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         intAgrs(array1);
+        System.out.println();
         intAgrs1(1, 2, 3, 4, 5, 6, 7, 8);
+        System.out.println();
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+        for (int a : array1) {//Foreach loop
+            System.out.print(a + " ");
+        }
     }
 }
