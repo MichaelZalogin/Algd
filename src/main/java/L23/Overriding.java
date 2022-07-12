07cbe72a6e3bfbffcd557e11e36ec00565a6566f
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-13 01:06:19 +0300

    Lesson 23. Overriding hiding final

--- src/main/java/L23/Overriding.java
+++ src/main/java/L23/Overriding.java
@@ -12,19 +12,24 @@ public class Overriding {
 
     public static void main(String[] args) {
         Overriding ov1 = new Overriding();
-        Animal a1 = new Mouse();
+        Mouse a1 = new Mouse();
         ov1.abc(a1);
         a1.getName();
+        System.out.println(a1.tail);
     }
 }
 
 class Animal {
+    static String tail = "One";
+
     void getName() {
         System.out.println("Animal");
     }
 }
 
 class Mouse extends Animal {
+    static int tail = 1;
+
     void getName() {
         System.out.println("Mouse");
     }
