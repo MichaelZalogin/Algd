b5183127e484ce61ccb039ed270072237dd24134
Michael Zalogin <zaloginmiha@gmail.com>
2022-07-20 16:24:51 +0300

    Lesson 26.

---
+++ src/main/java/OCA/L26/WrapperClass.java
@@ -0,0 +1,22 @@
+package OCA.L26;
+
+import java.util.ArrayList;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(5);
+        Long l = 50L; //Autoboxing
+        int a = list1.get(0);
+        Integer i1 = new Integer(3);
+
+        switch (i1) {
+            case 2:
+                System.out.println("i2");
+        }
+
+        String s1 = "50";
+        int i3 = Integer.parseInt(s1);//Парсинг в примитив
+        Integer i4 = i3;
+    }
+}
