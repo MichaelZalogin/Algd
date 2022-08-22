e32e9083f73b583f475a6fad9fc57357ee9e2d8f
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-23 01:28:01 +0300

    ArrayList

---
+++ src/main/java/javaCourseBlackBelt/collections/ArrayListEx1.java
@@ -0,0 +1,14 @@
+package javaCourseBlackBelt.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<String>();
+        arrayList1.add("Ivan");
+        arrayList1.add("Marya");
+        arrayList1.add("Petr");
+        arrayList1.add("Pavel");
+        System.out.println(arrayList1);
+    }
+}
