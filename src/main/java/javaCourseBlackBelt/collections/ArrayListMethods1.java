ec97b470109edc2899dae40b130b0a5f1f8e6d6e
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-23 02:33:20 +0300

    ArrayList

---
+++ src/main/java/javaCourseBlackBelt/collections/ArrayListMethods1.java
@@ -0,0 +1,25 @@
+package javaCourseBlackBelt.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMethods1 {
+    public static void main(String[] args) {
+        List<String> arrayList1 = new ArrayList<String>();
+        arrayList1.add("Ivan");
+        arrayList1.add("Marya");
+        arrayList1.add("Petr");
+        arrayList1.add("Pavel");
+        arrayList1.add(1, "Misha");
+        for (String s : arrayList1) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        System.out.println(arrayList1.get(2));
+        System.out.println(arrayList1.size());
+        arrayList1.set(1, "Victor"); // replace
+        System.out.println(arrayList1);
+        arrayList1.remove("Victor");
+        System.out.println(arrayList1);
+    }
+}
---
+++ src/main/java/javaCourseBlackBelt/collections/ArrayListMethods2.java
@@ -0,0 +1,55 @@
+package javaCourseBlackBelt.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class Employee {
+    int id;
+    String name;
+    String surname;
+    int salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && salary == employee.salary && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
+    }
+}
+
+public class ArrayListMethods2 {
+    public static void main(String[] args) {
+        List<Employee> list1 = new ArrayList<>();
+        Employee emp1 = new Employee(21, "Ivan", "Treg", 21243);
+        Employee emp2 = new Employee(15, "Pavel", "Petrov", 4334);
+        Employee emp3 = new Employee(123, "Victor", "Sidorov", 8142);
+        Employee emp4 = new Employee(321, "Nikolay", "Ivanov", 3232);
+        list1.add(emp1);
+        list1.add(emp2);
+        list1.add(emp3);
+        list1.add(emp4);
+        System.out.println(list1);
+        Employee emp5 = new Employee(321, "Nikolay", "Ivanov", 3232);
+        list1.remove(emp5);
+        System.out.println(list1);
+    }
+}
