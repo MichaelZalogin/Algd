11681bdabe565cc56b3527bf76a9d1bc2e932423
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-30 23:06:45 +0300

    Vector

--- src/main/java/javaCourseBlackBelt/collections/vector/VectorExp.java
+++ src/main/java/javaCourseBlackBelt/collections/vector/VectorExp.java
@@ -1,4 +1,15 @@
 package javaCourseBlackBelt.collections.vector;
 
+import java.util.Vector;
+
 public class VectorExp {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Michael");
+        vector.add("Igor");
+        vector.add("Oleg");
+        vector.add("Katya");
+        System.out.println(vector);
+        System.out.println(vector.firstElement());
+    }
 }
