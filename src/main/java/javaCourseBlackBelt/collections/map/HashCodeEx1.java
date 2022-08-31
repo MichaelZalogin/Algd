489d7fca9b8acaac2c81a482bf8c3e1fa42fbcf4
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-31 04:21:52 +0300

    HashMap

---
+++ src/main/java/javaCourseBlackBelt/collections/map/HashCodeEx1.java
@@ -0,0 +1,57 @@
+package javaCourseBlackBelt.collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashCodeEx1 {
+    public static void main(String[] args) {
+        Map<Student, Double> map = new HashMap<>();
+        Student st1 = new Student("Ivan", "Petrov", 3);
+        Student st2 = new Student("Marya", "Andreeva", 1);
+        Student st3 = new Student("Sergey", "Sidorov", 4);
+        map.put(st1, 7.5);
+        map.put(st2, 8.7);
+        map.put(st3, 9.2);
+        System.out.println(map);
+        Student st4 = new Student("Marya", "Andreeva", 1);
+        boolean result = map.containsKey(st4);
+        System.out.println(result);
+        System.out.println(st1.hashCode());
+        Student st5 = new Student("Igor", "Voronov", 4);
+    }
+}
+
+class Student {
+    String name;
+    String surname;
+    int course;
+
+    public Student(String name, String surname, int course) {
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, course);
+    }
+}
