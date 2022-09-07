1630a3cd3bf48c2aba84a0fadcc269bc2663b98c
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-07 21:56:52 +0300

    Nested classes

---
+++ src/main/java/javaCourseBlackBelt/nested_classes/Test.java
@@ -0,0 +1,14 @@
+package javaCourseBlackBelt.nested_classes;
+
+public class Test {
+    static class A {
+    }
+
+    class B {
+    }
+
+    void method() {
+        class C {
+        }
+    }
+}
