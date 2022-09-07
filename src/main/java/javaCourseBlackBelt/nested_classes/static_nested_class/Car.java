0c4002b9f664b05c4fd19ebf83deab97837009c0
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-07 22:14:25 +0300

    Static nested classes

---
+++ src/main/java/javaCourseBlackBelt/nested_classes/static_nested_class/Car.java
@@ -0,0 +1,44 @@
+package javaCourseBlackBelt.nested_classes.static_nested_class;
+
+public class Car {
+    String color;
+    int doorCount;
+    Engine engine;
+
+    public Car(String color, int doorCount, Engine engine) {
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "My car: {" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public static class Engine {
+        int hoursePower;
+
+        public Engine(int hoursePower) {
+            this.hoursePower = hoursePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My engine{" +
+                    "hoursePower=" + hoursePower +
+                    '}';
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(256);
+        System.out.println(engine);
+    }
+}
