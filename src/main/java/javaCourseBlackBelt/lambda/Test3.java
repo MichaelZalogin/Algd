3767dafaeef08748e061e7f7d6bd612bada01095
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-18 04:25:51 +0300

    Functional interface Supplier

---
+++ src/main/java/javaCourseBlackBelt/lambda/Test3.java
@@ -0,0 +1,42 @@
+package javaCourseBlackBelt.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Test3 {
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> arrayList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            arrayList.add(carSupplier.get());
+        }
+        return arrayList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> ourCars = createThreeCars(() ->
+                new Car("A8", "Black", 2));
+        System.out.println(ourCars);
+    }
+}
+
+class Car {
+    String model;
+    String color;
+    double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
