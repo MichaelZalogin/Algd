06a51cb4923fe40662739fb27637423495b8cec3
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-28 01:09:23 +0300

    Stream Count

--- src/main/java/javaCourseBlackBelt/stream/StreamDistinct.java
+++ src/main/java/javaCourseBlackBelt/stream/StreamDistinct.java
@@ -6,5 +6,9 @@ public class StreamDistinct {
     public static void main(String[] args) {
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
         stream.distinct().forEach(System.out::println);
+        System.out.println("===========");
+
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
+        System.out.println(stream2.count());
     }
 }
