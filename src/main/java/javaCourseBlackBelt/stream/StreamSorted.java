698b561f8cdda89080cb41b599ae81d10c1dd683
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-28 01:04:50 +0300

    Stream Sorted

--- src/main/java/javaCourseBlackBelt/stream/StreamSorted.java
+++ src/main/java/javaCourseBlackBelt/stream/StreamSorted.java
@@ -7,5 +7,14 @@ public class StreamSorted {
         int[] array = {3, 8, 1, 5, 9, 12, 21, 81, 7, 18};
         array = Arrays.stream(array).sorted().toArray();
         System.out.println(Arrays.toString(array));
+
+
+        int result1 = Arrays.stream(array).filter(e -> e % 2 == 1).map(e -> {
+            if (e % 3 == 0) {
+                e /= 3;
+            }
+            return e;
+        }).reduce((a, e) -> a + e).getAsInt();
+        System.out.println(result1);
     }
 }
