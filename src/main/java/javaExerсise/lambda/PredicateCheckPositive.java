81688bc408598b69b1742cebbbacad79289dbc1d
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-10 02:45:11 +0300

    1.1. Predicate. Проверить, что число положительное

---
+++ src/main/java/javaExerсise/lambda/PredicateCheckPositive.java
@@ -0,0 +1,12 @@
+package javaExerсise.lambda;
+
+public class PredicateCheckPositive {
+//    public static boolean check(int num) {
+//
+//        return check( (s) -> s > 0);
+//    }
+//
+//    private static boolean check(Predicate<Integer> predicate, int num) {
+//        return predicate.test(num);
+//    }
+}
