7b214e2425964ab82c3d507dd2c53f25ba8a33f2
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-10 23:10:52 +0300

    Predicate. Проверить, что число четное

---
+++ src/main/java/javaExerсise/lambda/PredicateCheckEven.java
@@ -0,0 +1,14 @@
+package javaExerсise.lambda;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckEven {
+
+    public static boolean check(int num) {
+        return check(s -> s % 2 == 0, num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
---
+++ src/test/java/javaExerсise/lambda/PredicateCheckEvenTest.java
@@ -0,0 +1,15 @@
+package javaExerсise.lambda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PredicateCheckEvenTest {
+
+    @Test
+    public void test() {
+        assertTrue(PredicateCheckEven.check(2));
+        assertFalse(PredicateCheckEven.check(1));
+    }
+
+}
