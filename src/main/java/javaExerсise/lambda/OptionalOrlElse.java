f1caa7bb662167e384441fb9cb8b18bad6ffd940
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-13 01:49:57 +0300

    5.4. Optional. Методы orElse(), orElseGet()

--- src/main/java/javaExerсise/lambda/OptionalOrlElse.java
+++ src/main/java/javaExerсise/lambda/OptionalOrlElse.java
@@ -4,10 +4,6 @@ import java.util.Optional;
 
 public class OptionalOrlElse {
     public static Integer orElse(Optional<Integer> optional) {
-        Integer value = -1;
-        if (optional.isPresent()) {
-            value = optional.get();
-        }
-        return value;
+        return optional.orElse(-1);
     }
 }
