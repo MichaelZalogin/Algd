5371fb8521946af5cbdde5adb7a9ff8cce89ffc6
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-20 23:17:50 +0300

    Создание стрима. Метод stream()

---
+++ src/main/java/javaExerсise/streamAPI/StreamMethod.java
@@ -0,0 +1,11 @@
+package javaExerсise.streamAPI;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class StreamMethod {
+
+    public static Stream<Integer> createStream(Collection<Integer> data) {
+        return data.stream();
+    }
+}
