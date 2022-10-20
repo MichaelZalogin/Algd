9ec5ddebfd628682517c9dc4a6ba185892777ac3
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-20 23:23:48 +0300

    1.3. Создание стрима из массива. Метод Arrays.stream()

---
+++ src/main/java/javaExerсise/streamAPI/ArraysStreamMethod.java
@@ -0,0 +1,10 @@
+package javaExerсise.streamAPI;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ArraysStreamMethod {
+    public static Stream<Integer> createStream(Integer[] data) {
+        return Arrays.stream(data);
+    }
+}
