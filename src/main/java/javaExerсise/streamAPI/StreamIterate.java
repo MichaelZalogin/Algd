b6848112ae08d4552eb73161932dce682f7d8566
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-20 23:29:15 +0300

    1.4. Создание стрима. Метод Stream.iterate()

---
+++ src/main/java/javaExerсise/streamAPI/StreamIterate.java
@@ -0,0 +1,10 @@
+package javaExerсise.streamAPI;
+
+import java.util.stream.Stream;
+
+public class StreamIterate {
+    public static void showArray(Integer[] data) {
+        Stream.iterate(0, i -> i < 4, i -> i + 2)
+                .forEach(i -> System.out.println(data[i]));
+    }
+}
