f18c2be3ce053e5d41f586993c3e4fafd4a324bf
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-24 23:52:45 +0300

    2.3. Понятие редукции. Метод reduce(). Редукция с начальным значением

---
+++ src/main/java/javaExerсise/streamAPI/ReductionWithTwoArguments.java
@@ -0,0 +1,9 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+
+public class ReductionWithTwoArguments {
+    public static Integer collect(List<Integer> list) {
+        return list.stream().reduce(5, (n1, n2) -> n1 * n2);
+    }
+}
