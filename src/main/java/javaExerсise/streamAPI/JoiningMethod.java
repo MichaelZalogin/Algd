7ba9efd424cdad89b4b03a3ff2fa17a0981c8587
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-25 01:08:00 +0300

    3.5. Сборка элементов. Сборка в строку

---
+++ src/main/java/javaExerсise/streamAPI/JoiningMethod.java
@@ -0,0 +1,11 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JoiningMethod {
+    public static String join(List<String> strings) {
+        return strings.stream().
+                collect(Collectors.joining(System.lineSeparator(), "Prefix", "Suffix"));
+    }
+}
