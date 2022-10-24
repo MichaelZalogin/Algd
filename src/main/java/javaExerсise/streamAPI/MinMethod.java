0a401aceb6a6a3092649692eec53e8372cda0c03
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-24 23:57:16 +0300

    2.4. Понятие редукции. Метод min(). Поиск кратчайшей строки

---
+++ src/main/java/javaExerсise/streamAPI/MinMethod.java
@@ -0,0 +1,10 @@
+package javaExerсise.streamAPI;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MinMethod {
+    public static String min(List<String> list) {
+        return list.stream().min(Comparator.comparingInt(String::length)).get();
+    }
+}
