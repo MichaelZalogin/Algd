61865d2312fdb49a5be79f50ccf31ce40aa96b45
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-01 01:30:50 +0300

    5.6. Терминальные операции. Метод findFirst()

---
+++ src/main/java/javaExerсise/streamAPI/FindFirstMethod.java
@@ -0,0 +1,10 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FindFirstMethod {
+    public static Optional<Integer> first(List<Integer> list) {
+        return list.stream().findFirst();
+    }
+}
