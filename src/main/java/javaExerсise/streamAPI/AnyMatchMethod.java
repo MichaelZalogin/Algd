eb47a8ba1218f4c98b761326ca82ba1338d8b1f5
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-01 01:20:49 +0300

    5.3. Терминальные операции. Метод anyMatch()

---
+++ src/main/java/javaExerсise/streamAPI/AnyMatchMethod.java
@@ -0,0 +1,9 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+
+public class AnyMatchMethod {
+    public static boolean check(List<String> data) {
+        return data.stream().anyMatch(s -> s.startsWith("job4j"));
+    }
+}
