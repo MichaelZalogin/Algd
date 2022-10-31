1b84cb92933810078d83e5c9f5d9d2aa26ba7746
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-01 01:38:41 +0300

    6.2. Промежуточные операции. Метод map(). Map с объектами

---
+++ src/main/java/javaExerсise/streamAPI/UserMap.java
@@ -0,0 +1,26 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserMap {
+
+    public static class User {
+
+        private int age;
+
+        public User(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
+    public static List<User> map(List<Integer> ages) {
+        return ages.stream()
+                .map(a -> new User(a))
+                .collect(Collectors.toList());
+    }
+}
