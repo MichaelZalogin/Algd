f4dd62788d175bd4cafe711b9e919dbcd47ce960
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-04 23:14:14 +0300

    6.5. Промежуточные операции. Метод boxed().

---
+++ src/main/java/javaExerсise/streamAPI/BoxedMethod.java
@@ -0,0 +1,13 @@
+package javaExerсise.streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BoxedMethod {
+    public static List<Integer> boxed(int[] data) {
+        return Arrays.stream(data)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
