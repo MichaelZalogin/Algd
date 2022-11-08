a24ffd291a7dc3043784cc2c9c21aaa18f206c34
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-08 23:29:54 +0300

    6.13. Промежуточные операции. Метод skip()

---
+++ src/main/java/javaExerсise/streamAPI/SkipMethod.java
@@ -0,0 +1,12 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SkipMethod {
+    public static List<String> lastThree(List<String> strings) {
+        return strings.stream()
+                .skip(2)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/javaExerсise/streamAPI/SkipMethodTest.java
@@ -0,0 +1,18 @@
+package javaExerсise.streamAPI;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SkipMethodTest {
+
+    @Test
+    public void test() {
+        assertEquals(
+                List.of("C", "D", "E"),
+                SkipMethod.lastThree(List.of("A", "B", "C", "D", "E"))
+        );
+    }
+}
