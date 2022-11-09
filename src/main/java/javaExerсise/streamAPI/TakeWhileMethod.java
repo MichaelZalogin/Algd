63bc383730b9f72e29c84725a6c0b13f8616d8b4
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-09 23:49:24 +0300

    6.16. Промежуточные операции. Метод takeWhile()

---
+++ src/main/java/javaExerсise/streamAPI/TakeWhileMethod.java
@@ -0,0 +1,13 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TakeWhileMethod {
+    public static List<Integer> takeWhile(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .takeWhile(s -> s < 0)
+                .collect(Collectors.toList());
+    }
+}
