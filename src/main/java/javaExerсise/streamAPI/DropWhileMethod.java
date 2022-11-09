659f1f1762f9f20fbd49d268056794a6e338160d
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-09 23:54:28 +0300

    6.18. Промежуточные операции. Метод dropWhile()

---
+++ src/main/java/javaExerсise/streamAPI/DropWhileMethod.java
@@ -0,0 +1,13 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DropWhileMethod {
+    public static List<Integer> dropWhile(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .dropWhile(s -> s < 0)
+                .collect(Collectors.toList());
+    }
+}
