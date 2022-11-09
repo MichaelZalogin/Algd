efc9699062aa25d775d4eeb215ad9f9828de7609
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-09 23:46:28 +0300

    6.15. Промежуточные операции. Метод sorted()

---
+++ src/main/java/javaExerсise/streamAPI/SortedMethod.java
@@ -0,0 +1,12 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortedMethod {
+    public static List<String> sorted(List<String> strings) {
+        return strings.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
