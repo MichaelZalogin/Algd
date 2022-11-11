1b3091bf9da9a087953df162ee0f87d5f87411da
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-11 23:36:37 +0300

    7.1. Optional и Stream. Метод flatMap()

---
+++ src/main/java/javaExerсise/streamAPI/OptionalFlatMap.java
@@ -0,0 +1,11 @@
+package javaExerсise.streamAPI;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OptionalFlatMap {
+    public static Optional<Integer> flatMap(List<String> strings) {
+        return null;
+    }
+}
