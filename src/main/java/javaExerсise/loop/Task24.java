c7cf95515ad2e5b4671159a9e7e64ca84b52437c
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-14 23:55:26 +0300

    23. Вывести ряд чисел с последующим увеличением

--- src/main/java/javaExerсise/loop/Task24.java
+++ src/main/java/javaExerсise/loop/Task24.java
@@ -5,7 +5,7 @@ import java.util.StringJoiner;
 public class Task24 {
     public static void loop(int num) {
         StringJoiner stringJoiner = new StringJoiner(" ");
-        for (int i = num - 10; i < num - 30; i++) {
+        for (int i = num - 10, b = 0; b < 5; i = i - 6, b++) {
             stringJoiner.add(String.valueOf(i));
         }
         System.out.println(stringJoiner);
