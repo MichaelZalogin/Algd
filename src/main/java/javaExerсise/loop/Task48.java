5b3880a7c19f8b72942128d04e11f240ff78bfd1
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-27 23:08:34 +0300

    48. Вывести серию чисел от 1 до N с разделителем

---
+++ src/main/java/javaExerсise/loop/Task48.java
@@ -0,0 +1,13 @@
+package javaExerсise.loop;
+
+import java.util.StringJoiner;
+
+public class Task48 {
+    public static void loop(int num) {
+        StringJoiner stringJoiner = new StringJoiner("+");
+        for (int i = 1; i <= num; i++) {
+            stringJoiner.add(String.valueOf(i));
+        }
+        System.out.println(stringJoiner);
+    }
+}
