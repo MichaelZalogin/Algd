1a5f7b1b4e00ccb264a07bfec35b3b061ca368e0
MichaelZalogin <zaloginmiha@gmail.com>
2022-11-29 23:24:39 +0300

    58. На какое количество двузначных чисел делится число

---
+++ src/main/java/javaExerсise/loop/Task58.java
@@ -0,0 +1,13 @@
+package javaExerсise.loop;
+
+public class Task58 {
+    public static void loop(int num) {
+        int count = 0;
+        for (int i = 10; i < 21; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
