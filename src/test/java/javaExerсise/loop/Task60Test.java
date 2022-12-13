b6635a0bb36da3a3e01d81e53a1e3f81888ad2ae
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-13 23:38:48 +0300

    60. Вывести двузначные числа с определенным условием

---
+++ src/test/java/javaExerсise/loop/Task60Test.java
@@ -0,0 +1,22 @@
+package javaExerсise.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Task60Test {
+    public String ln = System.lineSeparator();
+
+    @Test
+    public void whenTestThen6() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task60.loop();
+        String expected = "6" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}
