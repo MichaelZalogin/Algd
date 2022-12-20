8c7bf46b9e6dc41e574bc3c396acb2cacded8482
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-20 23:19:37 +0300

    71. Вывести сумму делителей числа

---
+++ src/main/java/javaExerсise/loop/Task71.java
@@ -0,0 +1,12 @@
+package javaExerсise.loop;
+
+public class Task71 {
+    public static void loop(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0)
+                sum += i;
+        }
+        System.out.println(sum);
+    }
+}
