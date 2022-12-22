48dcde75701762c32aa0efdefd38ac4dc5380f0e
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-22 23:38:50 +0300

    Обработать оценки учеников

---
+++ src/main/java/javaExerсise/loop/Task79.java
@@ -0,0 +1,16 @@
+package javaExerсise.loop;
+
+public class Task79 {
+    public static void loop(int[] grades) {
+        int bad = 0;
+        int sum = 0;
+        for (int i = 0; i < grades.length; i++) {
+            sum += grades[i];
+            if (grades[i] < 3) {
+                bad++;
+            }
+        }
+        double avg = (double) sum / grades.length;
+        System.out.println("Средняя оценка: " + avg + ", неуд. оценка: " + bad);
+    }
+}
