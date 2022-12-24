04b52d9cbd3dc9f2fb26b119482707e52e5792a3
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-24 04:10:17 +0300

    Вывести сумму членов арифметической прогрессии

---
+++ src/main/java/javaExerсise/loop/Task83.java
@@ -0,0 +1,11 @@
+package javaExerсise.loop;
+
+public class Task83 {
+    public static void loop(int num) {
+        double sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += Math.sqrt(2 * i - 1);
+        }
+        System.out.printf("%.2f", sum);
+    }
+}
