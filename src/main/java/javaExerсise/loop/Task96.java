5cb0add40db6bef8cfb54708521eca5784785feb
MichaelZalogin <zaloginmiha@gmail.com>
2022-12-30 03:01:16 +0300

    Найти наилучший результат

---
+++ src/main/java/javaExerсise/loop/Task96.java
@@ -0,0 +1,10 @@
+package javaExerсise.loop;
+
+import java.util.Arrays;
+
+public class Task96 {
+    public static void loop(int[] num) {
+        Arrays.sort(num);
+        System.out.println(num[0]);
+    }
+}
