5c301f78926c127fc6709ab3f92bca7341e88384
MichaelZalogin <zaloginmiha@gmail.com>
2023-01-01 16:31:52 +0300

    Среднее арифметическое

---
+++ src/main/java/javaExerсise/loop/Task98.java
@@ -0,0 +1,17 @@
+package javaExerсise.loop;
+
+public class Task98 {
+    public static void loop(int[] num) {
+        int max = num[0];
+        int min = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > max) {
+                max = num[i];
+            }
+            if (num[i] < min) {
+                min = num[i];
+            }
+        }
+        System.out.println((min + max) / 2);
+    }
+}
