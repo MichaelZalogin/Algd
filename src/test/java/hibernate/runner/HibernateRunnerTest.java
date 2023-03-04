4a2cb63d4613932320b4dc85c21c7b8b2cb74d3c
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-04 17:31:35 +0300

    How hibernate creates query

--- src/test/java/hibernate/runner/HibernateRunnerTest.java
+++ src/test/java/hibernate/runner/HibernateRunnerTest.java
@@ -6,17 +6,18 @@ import org.junit.jupiter.api.Test;
 import javax.persistence.Column;
 import javax.persistence.Table;
 import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HibernateRunnerTest {
 
     @Test
-    void checkReflectionApi() {
+    void checkReflectionApi() throws SQLException, IllegalAccessException {
         User user = User.builder()
                 .username("ivan@gmail.com")
                 .firstname("Ivan")
@@ -32,29 +33,37 @@ class HibernateRunnerTest {
                 values
                 (%s)
                 """;
-        /** Первый заполнитель определяем по полям @Table Entity, если она существуюет.
-         * Если нет, мапим имя класса
+        /** Первый заполнитель определяем по полям @Table Entity, если они существуют.
+         * Если нет, мапим имя класса.
          */
         String tableName = Optional.ofNullable(user.getClass().getAnnotation(Table.class))
                 .map(tableAnnotation -> tableAnnotation.schema() + "." + tableAnnotation.name())
                 .orElse(user.getClass().getName());
 
         /** Второй заполнитель. Получаем поля в массиве. Имя полей получаем либо из
-         * @Column по аргументу name, либо мапимся напрямую через имя поля
+         * @Column по аргументу name, либо мапимся напрямую через имя поля.
          */
-        var columnNames = user.getClass().getDeclaredFields();
-        Arrays.stream(columnNames)
+        Field[] declaredFields = user.getClass().getDeclaredFields();
+        String columnNames = Arrays.stream(declaredFields)
                 .map(field -> Optional.ofNullable(field.getAnnotation(Column.class))
                         .map(Column::name)
                         .orElse(field.getName()))
                 .collect(Collectors.joining(", "));
 
-        var columnValues = Arrays.stream(columnNames).map(field -> "?")
+        /** Третий заполнитель. Каждое поле из массива полей преобразовываем в "?" для
+         * использования в PrepareStatement.
+         */
+        String columnValues = Arrays.stream(declaredFields).map(field -> "?")
                 .collect(Collectors.joining(", "));
 
-        System.out.println(sql.formatted(tableName, columnNames, columnValues));
+        String sqlQuery = sql.formatted(tableName, columnNames, columnValues);
+        System.out.println(sqlQuery);
 
+//        Connection connection = null;
+//        PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+//        for (Field declaredField : declaredFields) {
+//            declaredField.setAccessible(true);
+//            preparedStatement.setObject(1, declaredField.get(user));
+//        }
     }
-
-
 }
